1e338c8813ffa8c4f48288cf2fecb2c5a7ebae2d
Andre-Margarido <andre_margarido@hotmail.com>
2024-12-02 19:13:13 +0000

    tentativa erro

--- code.js
+++ code.js
@@ -6,5 +6,13 @@ function fahrenheitToCelsius(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
 }
 
+let celsiusValue = 25; 
+let fahrenheitValue = 77;
+
 let fahrenheitResult = celsiusToFahrenheit(celsiusValue);
 let celsiusResult = fahrenheitToCelsius(fahrenheitValue);
+
+alert (
+  'Celsius to Fahrenheit: ${celsiusValue}°C equal to ${fahrenheitResult}°F'
+  'Fahrenheit para Celsius: ${fahrenheitValue}°F é igual a ${celsiusResult}°C'
+)
